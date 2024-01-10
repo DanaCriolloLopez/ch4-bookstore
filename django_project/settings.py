c63bfee2da38452aab990893eee4a1933fe1811c
Dana Criollo <dana.criollo.ljfj@gmail.com>
2024-01-10 17:24:29 -0500

    ch6

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django_project/settings.py
+LOGIN_REDIRECT_URL = "home" # new
+LOGOUT_REDIRECT_URL = "home" # new
